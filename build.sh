83136bb843bfe1a38a8167d9d6562dccdda12a7b
Mark A. Matienzo <matienzo@stanford.edu>
2017-08-28 23:14:01 -0700

    Fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e # halt script on error
-bundle exec jekyll build
+bundle exec jekyll build -d _site/schema-org
 bundle exec htmlproofer _site
-find _site/examples -type f -print0 | xargs -0 -n1 -I % sh -c 'printf %:\ ; bundle exec rdf validate %;'
+find _site/schema-org/examples -type f -print0 | xargs -0 -n1 -I % sh -c 'printf %:\ ; bundle exec rdf validate %;'
